80313ae47e93c28ff76180b807557df7fec49d46
Michael Mukolwe <michaelolukaka@gmail.com>
2024-03-07 12:03:37 +0300

    Github Finder pull users from github and display them

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
+import { Component } from 'react';
+import axios from 'axios';
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const { data } = await axios.get('https://api.github.com/users');
+
+    if (data) {
+      this.setState({ loading: false, users: data });
+    }
+  }
+
+  render() {
+    const { loading, users } = this.state;
+
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <Users loading={loading} users={users} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/users/Users.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import UserItem from './UserItem';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
+
+const Users = ({ loading, users }) => {
+  return (
+    <>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div style={userStyle}>
+          {users.map((user) => (
+            <UserItem key={user.id} user={user} />
+          ))}
+        </div>
+      )}
+    </>
+  );
+};
+
+Users.propTypes = {
+  loading: PropTypes.bool.isRequired,
+  users: PropTypes.array.isRequired,
+};
+
+const userStyle = {
+  display: 'grid',
+  gridTemplateColumns: 'repeat(3, 1fr)',
+  gridGap: '1rem',
+};
+
+export default Users;
