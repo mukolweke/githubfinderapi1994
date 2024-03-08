88226cb99c1f7c84c7f1bfbbdad542ce45389153
Michael Mukolwe <michaelolukaka@gmail.com>
2024-03-08 15:47:14 +0300

    Github Finder cleanup reducer function

--- src/context/github/githubReducer.js
+++ src/context/github/githubReducer.js
@@ -6,7 +6,7 @@ import {
   GET_REPOS,
 } from '../types';
 
-export default (state, action) => {
+const GithubReducer = (state, action) => {
   switch (action.type) {
     case SEARCH_USERS:
       return {
@@ -14,18 +14,21 @@ export default (state, action) => {
         users: action.payload,
         loading: false,
       };
+
     case GET_USER:
       return {
         ...state,
         user: action.payload,
         loading: false,
       };
+
     case CLEAR_USERS:
       return {
         ...state,
         users: [],
         loading: false,
       };
+
     case GET_REPOS: {
       return {
         ...state,
@@ -33,12 +36,16 @@ export default (state, action) => {
         loading: false,
       };
     }
+
     case SET_LOADING:
       return {
         ...state,
         loading: true,
       };
+
     default:
       return state;
   }
 };
+
+export default GithubReducer;
